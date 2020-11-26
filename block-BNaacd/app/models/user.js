98f5b270595db4c0c075cc90ef34ff433fad085c
tinkalguli <tinkalmunu@gmail.com>
2020-11-26 14:10:25 +0530

    Refactor

--- block-BNaacd/app/models/user.js
+++ block-BNaacd/app/models/user.js
@@ -1,6 +1,6 @@
 const { Schema } = require("mongoose");
 
-let articleSchema = new Schema({
+let userSchema = new Schema({
     name : String,
     email : { type : String, lowercase : true },
     age : { type : Number, default : 0 }
